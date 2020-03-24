683e55a283f9276cc5b38b76bffe87c3b8df2d9b
Anurag <anuragraj94@gmail.com>
2020-03-25 02:21:48 +0530

    Project Added

---
+++ SiteTest/Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿using SiteTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiteTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        static SqlConnection Con;
+        static SqlCommand Cmd;        
+        static SqlDataAdapter SqlDataAdapter;
+        static string ConString, Query;
+        static DataTable DataTable;
+        public static bool DbConnection()
+        {
+            try
+            {                
+                ConString = @"Data Source=DESKTOP-3UFDSE4\ANURAGSQL;Initial Catalog=DBModel;User ID=sa;Password=8299156008";             
+                Con = new SqlConnection(ConString);
+                Con.Open();
+                Con.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Con.Close();
+                return false;
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Create(UserModel data)
+        {
+            if (DbConnection())
+            {
+                Query = "insert into tbl_User values(@UserName,@Password)";
+                Cmd = new SqlCommand(Query, Con);                
+                Cmd.Parameters.AddWithValue("UserName", data.UserName);
+                Cmd.Parameters.AddWithValue("Password", data.Password);                
+                Con.Open();
+                Cmd.ExecuteNonQuery();
+                Con.Close();
+                return Json("Inserted", JsonRequestBehavior.AllowGet);
+            }    
+            return Json(null,JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Update(int ID)
+        {           
+            return View();
+        }
+        public JsonResult GetDataByID(int ID)
+        {           
+            if (DbConnection())
+            {
+                List<UserModel> listrs = new List<UserModel>();
+                DataTable = new DataTable();
+                Query = "select * from tbl_User where ID=" + ID + " ";
+                Cmd = new SqlCommand(Query, Con);
+                SqlDataAdapter = new SqlDataAdapter(Cmd);
+                SqlDataAdapter.Fill(DataTable);
+                foreach (DataRow dr in DataTable.Rows)
+                {
+                    listrs.Add(new UserModel
+                    {
+                        ID = Convert.ToInt32(dr["ID"]),
+                        UserName = dr["UserName"].ToString(),
+                        Password = dr["Password"].ToString(),
+                    });
+                }
+                return Json(listrs, JsonRequestBehavior.AllowGet);
+            }
+            return Json(null);
+        }
+        public JsonResult UpdateData(UserModel data)
+        {
+            if (DbConnection())
+            {                
+                Query = "update tbl_User set UserName='"+data.UserName+ "', Password='"+data.Password+"' where ID="+data.ID+" ";
+                Cmd = new SqlCommand(Query, Con);                
+                Con.Open();
+                Cmd.ExecuteNonQuery();
+                Con.Close();
+                return Json("Updated", JsonRequestBehavior.AllowGet);
+            }
+            return Json(null,JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Delete(int ID)
+        {
+            if (DbConnection())
+            {
+                UserModel data = new UserModel();
+                Query = "delete from tbl_User where ID=" + ID + " ";
+                Cmd = new SqlCommand(Query, Con);
+                Con.Open();
+                Cmd.ExecuteNonQuery();
+                Con.Close();
+                return Json("Deleted", JsonRequestBehavior.AllowGet);
+            }
+            return Json(null, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Show()
+        {
+            return View();
+        }
+        public JsonResult Get_data()
+        {
+            if (DbConnection())
+            {               
+                List<UserModel> listrs = new List<UserModel>();
+                DataTable = new DataTable();
+                Query = "select * from tbl_User";
+                Cmd = new SqlCommand(Query, Con);
+                SqlDataAdapter = new SqlDataAdapter(Cmd);
+                SqlDataAdapter.Fill(DataTable);
+                foreach (DataRow dr in DataTable.Rows)
+                {
+                    listrs.Add(new UserModel
+                    {
+                        ID = Convert.ToInt32(dr["ID"]),
+                        UserName = dr["UserName"].ToString(),
+                        Password = dr["Password"].ToString(),
+                    });
+                }
+                return Json(listrs, JsonRequestBehavior.AllowGet);
+            }
+            return Json(null);
+        }
+    }
+}
---
+++ SiteTest/Scripts/AngularController/HomeAngularJS.js
@@ -0,0 +1,73 @@
+﻿var app = angular.module("Create", []);
+
+app.controller("Controller", function ($scope, $http) {
+    $scope.btnSave = "Save";
+
+    // save data in database
+    $scope.Save = function () {
+        $scope.btnSave = "Please Wait....";
+        $http({
+            method: 'POST',
+            url: '/Home/Create',
+            data: $scope.UserModel
+        }).then(function (d) {            
+            $scope.btnSave = "Save";
+            $scope.UserModel = null;
+            alert(d.data);
+        }, function () {
+            alert('Failed');
+        });        
+    };
+
+    // show data in table
+    $http.get("/Home/Get_data").then(function (d) {
+        $scope.record = d.data;
+    }, function (error) {
+        alert('Failed');
+    });
+
+    // delete record by id 
+    $scope.deleterecord = function (id) {
+        $http({
+            method: 'POST',
+            url: '/Home/Delete',
+            data: { ID: id }
+        }).then(function (d) {
+            $scope.btnSave = "Save";
+            $scope.UserModel = null;            
+            $http.get("/Home/Get_data").then(function (d) {
+                $scope.record = d.data;
+            }, function (error) {
+                alert('Failed');
+            });
+        }, function () {
+            alert('Failed');
+        });
+    }
+
+    // Onload event
+    $scope.load = function (id) {
+        $http.get("/Home/GetDataByID?id="+id).then(function (d) {
+            $scope.UserModel = d.data[0];
+        }, function (error) {
+            alert('Failed');
+        });
+    }
+
+    // Update data in database
+    $scope.btnUpdate = "Update";
+    $scope.Update = function () {
+        $scope.btnSave = "Please Wait....";
+        $http({
+            method: 'POST',
+            url: '/Home/UpdateData',
+            data: $scope.UserModel
+        }).then(function (d) {
+            $scope.btnSave = "Update";
+            $scope.UserModel = null;
+            alert(d.data);
+        }, function () {
+            alert('Failed');
+        });       
+    };
+});
